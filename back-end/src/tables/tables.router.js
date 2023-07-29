5bda27fc18ccea248f44403138f3f170d2bf5580
Shane Pugmire <shaynstr@gmail.com>
2023-07-29 13:02:15 -0600

    created seat backend files

--- back-end/src/tables/tables.router.js
+++ back-end/src/tables/tables.router.js
@@ -8,7 +8,11 @@
  const controller = require("./tables.controller");
  const methodNotAllowed = require("../errors/methodNotAllowed");
  
- router.route("/").get(controller.list).post(controller.create).all(methodNotAllowed);
+ router
+    .route("/")
+    .get(controller.list)
+    .post(controller.create)
+    .all(methodNotAllowed);
  
  module.exports = router;
  
